a48728fce72bc24af3c67855905ade7d776ce8a0
Salik Anwar <135861792+imSalik-Anwar@users.noreply.github.com>
2024-02-12 10:14:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 function fibonacci(num) {
-	if(num == first) return 0;
-	if(num == second) return 1;
-	let first = 0;
-	let second = 1;
-	let third;
+	if(num == first) {
+		return 0
+	}
+	if(num == second){
+		 return 1
+	}
+	let first = 0
+	let second = 1
+	let third
 
 	for(let i = 3; i <= num; i++){
 		third = first + second
